6fbf34352de1267caece6f4137ff601fc1eeb89e
Justin Kramer <jkkramer@gmail.com>
2010-06-30 14:20:29 -0400

    code to support local, repl-based development

---
+++ src/clj_wiki/local.clj
@@ -0,0 +1,87 @@
+(ns clj-wiki.local
+  (:use [ring.adapter.jetty :only [run-jetty]]
+        [ring.middleware.file :only [wrap-file]]
+        [ring.middleware.file-info :only [wrap-file-info]]
+        [ring.util.response :only [redirect]])
+  (:import java.io.File
+           java.util.HashMap
+           [com.google.apphosting.api ApiProxy ApiProxy$Environment]
+           [com.google.appengine.tools.development
+            ApiProxyLocalFactory
+            LocalServerEnvironment]))
+
+(defonce server (atom nil))
+(def port 8181)
+(def login-info (atom {:logged-in? false
+                       :admin? false
+                       :email ""
+                       :auth-domain ""}))
+
+(defn login
+  ([email] (login email false))
+  ([email admin?] (swap! login-info merge {:email email
+                                           :logged-in? true
+                                           :admin? admin?})))
+
+(defn logout []
+  (swap! login-info merge {:email ""
+                           :logged-in? false
+                           :admin? false}))
+
+(defn- set-app-engine-environment []
+  "Sets up the App Engine environment for the current thread."
+  (let [att (HashMap. {"com.google.appengine.server_url_key"
+                       (str "http://localhost:" port)})
+        env-proxy (proxy [ApiProxy$Environment] []
+                    (isLoggedIn [] (:logged-in? @login-info))
+                    (getEmail [] (:email @login-info))
+                    (getAuthDomain [] (:auth-domain @login-info))
+                    (isAdmin [] (:admin? @login-info))
+                    (getRequestNamespace [] "")
+                    (getDefaultNamespace [] "")
+                    (getAttributes [] att)
+                    (getAppId [] "_local_"))]
+    (ApiProxy/setEnvironmentForCurrentThread env-proxy)))
+
+(defn- set-app-engine-delegate [dir]
+  "Initializes the App Engine services. Needs to be run (at least) per JVM."
+  (let [local-env (proxy [LocalServerEnvironment] []
+                    (getAppDir [] (File. dir))
+                    (getAddress [] "localhost")
+                    (getPort [] port)
+                    (waitForServerToStart [] nil))
+        api-proxy (.create (ApiProxyLocalFactory.) local-env)]
+    (ApiProxy/setDelegate api-proxy)))
+
+(defn init-app-engine
+  "Initializes the App Engine services and sets up the environment.
+  To be called from the REPL."
+  ([] (init-app-engine "/tmp"))
+  ([dir]
+     (set-app-engine-delegate dir)
+     (set-app-engine-environment)))
+
+(defn wrap-local-app-engine [app]
+  "Wraps a ring app to enable the use of App Engine Services."
+  (fn [req]
+    (set-app-engine-environment)
+    (app req)))
+
+(defn start-server [app]
+  "Initializes the App Engine services and (re-)starts a Jetty server
+   running the supplied ring app, wrapping it to enable App Engine API use
+   and serving of static files."
+  (set-app-engine-delegate "/tmp")
+  (swap! server (fn [instance]
+                   (when instance
+                     (.stop instance))
+                   (let [app (-> app
+                                 (wrap-local-app-engine)
+                                 (wrap-file "./war")
+                                 (wrap-file-info))]
+                     (run-jetty app {:port port
+                                     :join? false})))))
+
+(defn stop-server []
+  "Stops the local Jetty server."
+  (swap! server #(when % (.stop %))))

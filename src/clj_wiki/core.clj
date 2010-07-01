7bb2243f33b8f87b412ae12da1756682d10b4c9c
Justin Kramer <jkkramer@gmail.com>
2010-07-01 02:41:33 -0400

    page history, recent changes

--- src/clj_wiki/core.clj
+++ src/clj_wiki/core.clj
@@ -60,24 +60,43 @@
 (defn get-wiki-pages []
   (ds/find-all (ds/query "wiki-page")))
 
-(defn get-wiki-page [name]
-  (try
-    (let [page (ds/get-entity (ds/create-key "wiki-page" name))]
-      (assoc page :content (.getValue (:content page))))
-    (catch Exception _
-      nil)))
+(defn get-wiki-pages-by-date []
+  (-> (ds/query "wiki-page-history")
+      (ds/order-by :last-updated :desc)
+      (ds/find-all)))
+
+(defn get-wiki-page [name & [revision]]
+  (let [key (if revision
+              (ds/create-key "wiki-page-history" (str name " " revision))
+              (ds/create-key "wiki-page" name))]
+    (try
+      (let [page (ds/get-entity key)]
+        (assoc page :content (.getValue (:content page))))
+      (catch Exception _
+        nil))))
+
+(defn get-wiki-page-history [name]
+  (-> (ds/query "wiki-page-history")
+      (ds/filter-by = :name name)
+      (ds/order-by :last-updated :desc)
+      (ds/find-all)))
 
 (defn save-wiki-page [name content see]
-  (let [ui (users/user-info)]
-    (ds/create-entity {:kind "wiki-page"
-                       :key (ds/create-key "wiki-page" name)
-                       :name name
-                       :content (Text. content)
-                       :see see
-                       :last-updated (.getTime (java.util.Date.))
-                       :updated-by (if (:user ui)
-                                     (.getEmail (:user ui))
-                                     "Anonymous")})))
+  (let [ui (users/user-info)
+        rec {:name name
+             :content (Text. content)
+             :see see
+             :last-updated (.getTime (java.util.Date.))
+             :updated-by (if (:user ui)
+                           (.getEmail (:user ui))
+                           "Anonymous")}]
+    (ds/create-entity
+     (merge rec {:kind "wiki-page"
+                 :key (ds/create-key "wiki-page" name)}))
+    (ds/create-entity
+     (merge rec {:kind "wiki-page-history"
+                 :key (ds/create-key "wiki-page-history"
+                                     (str name " " (:last-updated rec)))}))))
 
 ;; layout / rendering
 
@@ -86,10 +105,17 @@
   (.markdown markdown-processor
              (-> txt
                  (.replace "<script" "")
+                 ;; code block
                  (.replaceAll "~{3,}((?:.|[\r\n])+?)~{3,}"
                               "<pre class=\"code\">$1</pre>")
+                 ;; output block
                  (.replaceAll ">{3,}((?:.|[\r\n])+?)>{3,}"
                               "<pre class=\"output\">$1</pre>"))))
+
+(defn render-timestamp [ts]
+  (when ts
+    (.format (java.text.SimpleDateFormat. "d MMM yyyy, hh:mm aaa z")
+             (java.util.Date. ts))))
 
 (defn render-session-info []
   (let [ui (users/user-info)]
@@ -107,7 +133,10 @@
    [:h3 "Namespaces"]
    [:ul#ns-list
     (for [ns ns-list]
-      [:li (link-to (uri ns) ns)])]])
+      [:li (link-to (uri ns) ns)])]
+   [:h3 "Meta"]
+   [:ul#meta
+    [:li (link-to (uri "changes") "Recent Changes")]]])
 
 (defn render-page [title & body]
   "Render a page using the given title and body. Title will be escaped,
@@ -135,49 +164,53 @@
        [:div#main-content body]
        [:div.clear]]]]]))
 
-(defn render-edit-form [page-name page]
-  [:form {:method "POST" :action (uri page-name)}
-   [:p "Examples:"]
-   [:textarea {:id "edit-text" :name "edit-text"} (h (:content page))]
-   [:p "See also (one function per line, namespace-qualified):"]
-   [:textarea {:id "see" :name "see"} (h (:see page))]
-   [:input.button {:type "submit" :value "Save"}]])
-
-(defn render-timestamp [ts]
-  (when ts
-    (.format (java.text.SimpleDateFormat. "d MMM yyyy, hh:mm aaa z")
-             (java.util.Date. ts))))
-
-(defn render-wiki-page [page-name page edit?]
+(defn render-wiki-page-edit-form [page-name page]
   (render-page
    page-name
    (html
-    (when (and (not edit?) (pos? (count page-name)))
-      (html
-       (when-let [arglists (:arglists (meta (resolve (symbol page-name))))]
-         [:p#arglists (str arglists)])
-       (when-let [doc (:doc (meta (resolve (symbol page-name))))]
-         [:p#doc (.replace doc "\n\n" "<br><br>")])))
-    (if edit?
-      (render-edit-form page-name page)
-      (if (nil? (:content page))
-        [:p.empty "[No examples]"]
-        [:div#examples
-         [:h3 "Examples"]
-         (render-markdown (:content page))]))
-    (when-not edit?
-      (html
-       (when (pos? (count (:see page)))
-         [:div#see-shell
-          [:h3 "See Also"]
-          [:ul#see (for [f (.split #"[\r\n]+" (:see page))]
-                     [:li (link-to (uri f) f)])]])
-       [:p#page-info
-        [:span#page-last-updated
-         "Last updated " (render-timestamp (:last-updated page))
-         " by " (:updated-by page)]
-        [:span#edit-link.button (link-to (uri page-name {:edit 1})
-                                         "Edit page")]])))))
+    [:form {:method "POST" :action (uri page-name)}
+     [:p "Examples:"]
+     [:textarea {:id "edit-text" :name "edit-text"} (h (:content page))]
+     [:p "See also (one function per line, namespace-qualified):"]
+     [:textarea {:id "see" :name "see"} (h (:see page))]
+     [:input.button {:type "submit" :value "Save"}]
+     [:span#cancel (link-to (uri page-name) "Cancel")]])))
+
+(defn render-wiki-page [page-name page & [edit? revision]]
+  (render-page
+   page-name
+   (html
+    (when revision
+      [:p#revision-notice "This is a revision from "
+       (render-timestamp (:last-updated page)) ". "
+       (link-to (uri page-name) "View current revision")])
+    (when-let [arglists (:arglists (meta (resolve (symbol page-name))))]
+      [:p#arglists (str arglists)])
+    (when-let [doc (:doc (meta (resolve (symbol page-name))))]
+      [:p#doc (.replace doc "\n\n" "<br><br>")])
+    (if (zero? (count (:content page)))
+      [:p.empty "[No examples]"]
+      [:div#examples
+       [:h3 "Examples"]
+       (render-markdown (:content page))])
+    (when (pos? (count (:see page)))
+      [:div#see-shell
+       [:h3 "See Also"]
+       [:ul#see (for [f (.split #"[\r\n]+" (:see page))]
+                  [:li (link-to (uri f) f)])]])
+    (when-not revision
+      [:p#page-info
+       [:span#edit-link.button (link-to (uri page-name {:edit 1})
+                                        "Edit page")]
+       (when (:last-updated page)
+         [:span#page-last-updated
+          (link-to (uri page-name {:history 1})
+                   "Last updated " (render-timestamp (:last-updated page))
+                   " by " (:updated-by page))])]))))
+
+(defn render-wiki-page-history [page-name page]
+  (render-wiki-page-changes (get-wiki-page-history page-name)
+                            (str page-name " History")))
 
 (defn render-wiki-page-list []
   (let [pages (sort-by :name (get-wiki-pages))]
@@ -187,6 +220,19 @@
       [:ul#page-list
        (for [page pages]
          [:li (link-to (uri (:name page)) (:name page))])]))))
+
+(defn render-wiki-page-changes [pages & [title]]
+  (render-page
+   (or title "Recent Changes")
+   (html
+    [:table#changes
+     [:tr [:th "Page"] [:th "When"] [:th "Who"]]
+     (for [page pages]
+       [:tr
+        [:td (link-to (uri (:name page) {:revision (:last-updated page)})
+                      (:name page))]
+        [:td (render-timestamp (:last-updated page))]
+        [:td (:updated-by page)]])])))
 
 (defn render-ns-list [ns]
   (render-page
@@ -203,26 +249,37 @@
   (redirect (uri page-name)))
 
 (defn wiki-handler [req]
-  (let [page-name (url-decode (subs (:uri req) 1))
-        page (get-wiki-page page-name)]
-    (cond
-     (when (pos? (count page-name))
-       (try (ns-publics (symbol page-name))
-            (catch Exception _ nil)))
-     (response (render-ns-list page-name))
-     
-     (= "list" page-name)
-     (response (render-wiki-page-list))
-     
-     (= :post (:request-method req))
-     (save-handler req page-name page)
+  (let [params (:query-params req)
+        page-name (url-decode (subs (:uri req) 1))
+        page (get-wiki-page page-name (params "revision"))]
+    (if (= :post (:request-method req))
+      (save-handler req page-name page)
+      (response
+       (cond
+        
+        (zero? (count page-name))
+        (response (render-page "Home" [:p "Nothing here yet!"]))
 
-     (zero? (count page-name))
-     (response (render-page "Home" [:p "Nothing here yet!"]))
+        (when (pos? (count page-name))
+          (try (ns-publics (symbol page-name))
+               (catch Exception _ nil)))
+        (render-ns-list page-name)
      
-     :else
-     (response (render-wiki-page page-name page
-                                 ((:query-params req) "edit"))))))
+        (= "list" page-name)
+        (render-wiki-page-list)
+
+        (= "changes" page-name)
+        (render-wiki-page-changes (get-wiki-pages-by-date))
+     
+        (params "history")
+        (render-wiki-page-history page-name page)
+
+        (params "edit")
+        (render-wiki-page-edit-form page-name page)
+        
+        :else
+        (render-wiki-page page-name page
+                          (params "edit") (params "revision")))))))
 
 (def wrapped-wiki-handler (-> wiki-handler
                               wrap-params
